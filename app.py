0e3b9a61ea7c50ed85979efcea5d0b3214e18af9
Yerninti Revathi <reeshmayerninti@gmail.com>
2025-09-03 21:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def update_vehicle_availability(vehicle_id):
 # Routes
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 # ---- Check if User is Logged In ----
 @app.route('/is_logged_in')
